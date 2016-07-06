39b50f18b4a039ea3aca6673569f243b2c345a06
Chris Rodriguez <github@chrisrodriguez.me>
2016-07-06 08:38:09 -0400

    Fixing Python test

--- lms/djangoapps/instructor/tests/views/test_instructor_dashboard.py
+++ lms/djangoapps/instructor/tests/views/test_instructor_dashboard.py
@@ -170,7 +170,7 @@ class TestInstructorDashboard(ModuleStoreTestCase, LoginEnrollmentTestCase, XssT
         response = self.client.get(self.url)
 
         # Check that the number of professional enrollments is two
-        self.assertContains(response, "<td>Professional</td><td>2</td>")
+        self.assertContains(response, '<th scope="row">Professional></th><td>2</td>')
 
     @patch.dict(settings.FEATURES, {'DISPLAY_ANALYTICS_ENROLLMENTS': False})
     @override_settings(ANALYTICS_DASHBOARD_URL='http://example.com')
